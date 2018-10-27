3aaa18a9b5d9f5459ed448d74185383a216c23e4
Geerocktricks <geerockface4@gmail.com>
2018-10-27 23:34:17 +0300

    This commit initiates my search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule
