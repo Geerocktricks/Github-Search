e9f95fadec857f0e56488e9e2d90e548a7cca428
Geerocktricks <geerockface4@gmail.com>
2018-10-28 13:10:26 +0300

    This commit will add my clientId & clientSecrets to my profile class

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Http , Headers } from '@angular/http' ;
+import 'rxjs/add/operator/maps';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+
+// i will export this to profile ts (class)
+  private username: string;
+  private clientId = 'f190063789417e8c6ca9';
+  private clientSectret = '1ccd3fd56ada87a5ab0d0b037339e679cfe6d351';
+
+
+  constructor(_http: Http) {
+
+   }
 }
